260fa6112a9fe9a57ddc0c83089bcd742d39901c
ohadpartuck <ohadpartuck@users.noreply.github.com>
2014-04-22 17:13:35 +0300

    Update fog.rb

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'AKIAJYEKHY3FIKKMYH7Q',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => 'XXXX', #removed this secret
+      :aws_secret_access_key  => 'XXXX',
   }
   config.fog_directory  = 'fiverr-cool'
   config.fog_public     = false
